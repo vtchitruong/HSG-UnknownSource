5d281fb96b21e1d7fbebb6c6c4978759a492863e
Vo Thach Chi Truong <vtchitruong@gmail.com>
2024-02-04 13:34:53 +0700

    Add Python code and modify some CPP

--- XoSo/xoso.cpp
+++ XoSo/xoso.cpp
@@ -13,13 +13,11 @@ typedef long long int lli;
 
 int n, k;
 vector<int> a;
-vector<vector<int>> choices; // Các cách chọn bộ k số
+vector<vector<int>> choices; // Mảng chứa các cách chọn bộ k số
+
 
 void Input()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-
     ifstream f;
     f.open(inputFile);
 
@@ -35,11 +33,11 @@ void Input()
     f.close();
 }
 
+
 // Hàm tạo bộ k số từ n số bằng back-tracking và đệ quy
 void Generate(vector<int>& choice, int next)
 {
-    // Nếu một bộ đã có đủ k số thì dừng đệ quy
-    // và nạp vào vector choices
+    // Nếu một bộ đã có đủ k số thì nạp vào choices và dừng đệ quy
     if (choice.size() == k)
     {
         choices.push_back(choice);
@@ -58,19 +56,24 @@ void Generate(vector<int>& choice, int next)
     }
 }
 
+
 lli Process()
 {
+    // Khởi tạo biến choice nhằm phục vụ cho việc gọi đệ quy
     vector<int> choice;
     Generate(choice, 0);
 
+    // Mảng chứa các điểm thưởng
     vector<int> bonus;
 
+    // Tính điểm thưởng của từng bộ số rồi nạp vào bonus
     for (int i = 0; i < choices.size(); ++i)
     {
         int m = *max_element(choices[i].begin(), choices[i].end());
         bonus.push_back(m);
     }
 
+    // Tính tổng điểm thưởng
     lli sum_bonus = (lli)accumulate(bonus.begin(), bonus.end(), 0);
     return sum_bonus % (lli)(10E9 + 7);
 }
@@ -79,9 +82,6 @@ void Output()
 {
     int res = Process();
 
-    ios_base::sync_with_stdio(false);
-    cout.tie(nullptr);
-
     ofstream f;
     f.open(outputFile);
 
@@ -89,6 +89,7 @@ void Output()
 
     f.close();
 }
+
 
 void Show_choices()
 {
@@ -101,6 +102,7 @@ void Show_choices()
         cout << endl;
     }
 }
+
 
 int main()
 {
---
+++ XoSo/xoso.py
@@ -0,0 +1,72 @@
+import os
+import sys
+
+input_file = os.path.join(sys.path[0], 'xoso.inp')
+output_file = os.path.join(sys.path[0], 'xoso.out')
+
+n = k = 0
+a = []
+choices = [] # Mảng chứa các cách chọn bộ k số
+
+
+def input_data():
+    global n, k, a
+    with open(input_file) as f:
+        n, k = map(int, f.readline().split())
+        a = list(map(int, f.readline().split()))
+
+
+# Hàm tạo bộ k số từ n số bằng back-tracking và đệ quy
+def generate(choice, next):
+    # Nếu một bộ đã có đủ k số thì nạp vào choices và dừng đệ quy
+    global a, choices
+    if len(choice) == k:
+        choices.append(choice.copy())
+        return
+
+    # Ứng với mỗi số x trong n số:
+    # - Nạp số x này vào bộ đang xét chọn
+    # - Gọi đệ quy để nạp số tiếp theo vào vị trí tiếp theo của bộ đang xét chọn
+    # - Gỡ bỏ x ở vị trí cuối của bộ đang xét chọn 
+    for i in range(next, len(a)):
+        choice.append(a[i])
+        generate(choice, i + 1)
+        choice.pop()
+
+
+def process():
+    # Khởi tạo biến choice nhằm phục vụ cho việc gọi đệ quy
+    choice = []
+    generate(choice, 0)
+
+    # Mảng chứa các điểm thưởng
+    bonus = []
+
+    # Tính điểm thưởng của từng bộ số rồi nạp vào bonus
+    global choices
+    for i in range(len(choices)):
+        m = max(choices[i])
+        bonus.append(m)
+
+    # Tính tổng điểm thưởng
+    sum_bonus = sum(bonus, 0)
+    return sum_bonus
+
+
+def output():
+    res = str(process())
+
+    with open(output_file, 'w') as f:
+        f.write(res)
+
+
+def show_choices():
+    global choices
+    for choice in choices:
+        print(*choice)
+
+
+if __name__ == '__main__':
+    input_data()
+    output()
+    show_choices()
